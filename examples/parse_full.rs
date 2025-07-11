1753b497531018f3334d2ba5a68a21728eb9a3bd
naoki <2629936804@qq.com>
2025-07-11 21:21:07 +0800

    update

--- examples/parse_full.rs
+++ examples/parse_full.rs
@@ -23,23 +23,54 @@ fn main() {
     let clearinghouse = &perp_dexs_obj["clearinghouse"];
 
 
-    for key in clearinghouse.as_object().unwrap().keys() {
-        println!("{}", key);
+    let user_states = &clearinghouse["user_states"];
+
+    let user_to_state = user_states["user_to_state"].as_array().unwrap();
+
+    let user_0 = &user_to_state[0];
+    let (addr, value): (String, Value) = serde_json::from_value(user_0.clone()).unwrap();
+
+    let mut total_e = 0;
+    let mut total_s = 0;
+    let mut total_neg_s = 0;
+    let mut long_trades = 0;
+    let mut short_trades = 0;
+    for user in user_to_state {
+        let (addr, value): (String, Value) = serde_json::from_value(user.clone()).unwrap();
+
+        if let Some(p) = value.get("p") {
+            if let Some(p) = p.get("p") {
+                let positions = p.as_array().unwrap();
+                for pos in positions {
+                    let (asset_idx, pos_data): (u64, Value) = serde_json::from_value(pos.clone()).unwrap();
+
+                    if asset_idx == 1 {
+                        if let Some(s) = pos_data.get("s") {
+                            let s = s.as_i64().unwrap();
+
+                            if s < 0 {
+                                total_neg_s += s;
+                                short_trades += 1;
+                            } else {
+                                total_s += s;
+                                long_trades += 1;
+                            }
+                        }
+                        if let Some(e) = pos_data.get("e") {
+                            let e = e.as_i64().unwrap();
+                            total_e += e;
+                        }
+                    }
+                }
+            }
+        }
     }
 
-    // let mut file = File::create("data/books.json").unwrap();
-    // file.write_all(serde_json::to_string_pretty(&books).unwrap().as_bytes()).unwrap();
+    println!("total_s: {}", total_s);
+    println!("total_neg_s: {}", total_neg_s);
+    println!("total_e: {}", total_e);
 
-    // let mut file = File::create("data/clearinghouse.json").unwrap();
-    // file.write_all(serde_json::to_string_pretty(&clearinghouse).unwrap().as_bytes()).unwrap();
-
-    // let mut file = File::create("data/funding_tracker.json").unwrap();
-    // file.write_all(serde_json::to_string_pretty(&funding_tracker).unwrap().as_bytes()).unwrap();
-
-    // let mut file = File::create("data/twap_tracker.json").unwrap();
-    // file.write_all(serde_json::to_string_pretty(&twap_tracker).unwrap().as_bytes()).unwrap();
-
-    // let mut file = File::create("data/schema.json").unwrap();
-    // file.write_all(serde_json::to_string_pretty(&schema).unwrap().as_bytes()).unwrap();
+    println!("long_trades: {}", long_trades);
+    println!("short_trades: {}", short_trades);
 
 }
